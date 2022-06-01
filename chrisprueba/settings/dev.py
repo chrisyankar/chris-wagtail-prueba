391c68a991981fbfb742c664c6373a78784a6299
Christian Zamora <chris191188@gmail.com>
2022-06-01 15:25:28 -0400

    first commit

---
+++ chrisprueba/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-rxy&@*1l4s)2-4xe&is-$5#5!hm(^p4*p%h)hxge_-u7!1lm#0"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
